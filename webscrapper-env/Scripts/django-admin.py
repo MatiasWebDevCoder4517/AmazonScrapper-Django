3ab5ae87048290f78bba6b7b1af7e6b23281d03d
Matias <matiaswebdeveloper4517@gmail.com>
2022-02-28 04:10:31 -0300

    InitialCommit

---
+++ webscrapper-env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\matias\desktop\learn\build_practice\webscrapperamazon\webscrapper-env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
